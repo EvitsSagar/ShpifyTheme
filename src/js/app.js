b0d16c25a5a31a99897204831b5552b90351b96a
Admin <am488.evits@gmail.com>
2024-12-20 18:30:57 +0530

    add alpine js Intersect Plugin

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,15 @@
 import Alpine from 'alpinejs'
+import intersect from '@alpinejs/intersect'
  
+Alpine.plugin(intersect);
+
 window.Alpine = Alpine
  
 Alpine.start()
+
+
+// import Alpine from 'alpinejs'
+ 
+// window.Alpine = Alpine
+ 
+// Alpine.start()
